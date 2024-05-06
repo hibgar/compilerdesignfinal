99500f09f3d2f0655564b477d0d7e3486dd89ae9
tnaik <98431300+tnaik@users.noreply.github.com>
2024-05-06 13:23:28 -0700

    working on table

--- h72.cpp
+++ h72.cpp
@@ -4,6 +4,7 @@
 #include <stack>
 #include <string>
 #include <vector>
+#include <sstream>
 #include <unordered_map>
 using namespace std;
 
@@ -39,35 +40,99 @@ void displayStack(stack<char> stack) {
     }
 }
 
+std::vector<std::string> tokenize(const std::string& str) {
+    std::vector<std::string> tokens;
+    std::istringstream iss(str);
+    std::string token;
+    while (iss >> token) {
+        tokens.push_back(token);
+    }
+    return tokens;
+}
 
 
 int main() {
+
+    std::ifstream inputFile("finalv24.txt");
+    if (!inputFile.is_open()) {
+        std::cerr << "Error: Unable to open input file." << std::endl;
+        return 1;
+    }
+
+    std::string line;
+    while (std::getline(inputFile, line)) {
+        // Tokenize each line
+        std::vector<std::string> tokens = tokenize(line);
+        // Output each token
+        for (const auto& token : tokens) {
+            std::cout << token << std::endl;
+        }
+    }
   
     stack<char> compiler;
     string inputs[3] = {"(i+i)*i$", "i*(i-i)$", "i(i+i)$"};
 
-    int twodarray[5][8] = 
+    int twodarray[22][33] = 
     {
-       //i,+,-,*,/,(,),$
-        {1,0,0,0,0,1,0,0}, //E
-        {0,2,3,0,0,0,4,4}, //Q
-        {5,0,0,0,0,5,0,0}, //T
-        {0,4,4,6,7,0,4,4}, //R
-        {8,0,0,0,0,9,0,0}  //F
+       //0,1,2,3,4,5,6,7,8,9,p,q,r,s,+,-,*,/,=,l,program,var , ; : ( ) $ begin end. integer write "value=",
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0}, //P
+        {0,0,0,0,0,0,0,0,0,0,2,2,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}, //I
+        {3,3,3,3,3,3,3,3,3,3,2,2,2,2,4,4,4,4,0,0,0,0,4,4,4,0,4,0,0,0,0,0,0}, //PI
+        {0,0,0,0,0,0,0,0,0,0,5,5,5,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}, //DL
+        {0,0,0,0,0,0,0,0,0,0,7,7,7,7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}, //D
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,6,0,4,0,0,0,0,0,0,0,0}, //PD
+        {}, //T
     };
  // 0: reject, 1: TQ, 2: +TQ, 3: -TQ, 4: lambda, 5: FR, 6: *FR, 7: /FR, 8: i, 9: (E)
 
-    char key[][3] = {
-        {'Q','T'},
-        {'Q', 'T', '+'},
-        {'Q', 'T', '-'},
-        {'l'}, //lambda
-        {'R', 'F'},
-        {'R','F','*'},
-        {'R','F','/'},
-        {'i'},
-        {')', 'E', '('}
-    };
+
+    vector<vector<string>> key =
+    { 
+        // 0 for reject
+        {"program", "I", ";", "var", "DL", "begin", "SL", "end."}, //1
+        {"L", "PI"}, //2
+        {"X", "PI"}, //3
+        {"l"}, //4
+        {"D", ":", "T", ";"}, //5
+        {",", "D"}, //6
+        {"I", "PD"}, //7
+        {"integer"}, //8
+        {"S", "PS"}, //9
+        {"SL"}, //10
+        {"A", "S"}, //11
+        {"W"}, //12
+        {"write", "(", "R", "I", ")", ";"}, //13
+        {"\"value=\","}, //14
+        {"I", "=", "E", ";"}, //15
+        {"M", "PE"}, //16
+        {"+", "M", "PE"}, //17
+        {"-", "M", "PE"}, //18
+        {"F", "PM"}, //19
+        {"*", "F", "PM"}, //20
+        {"/", "F", "PM"}, //21
+        {"I"}, //22
+        {"N"}, //23
+        {"(","E",")"}, //24
+        {"S", "X", "PN"}, //25
+        {"X", "PN"}, //26
+        {"+"}, //27
+        {"-"}, //28
+        {"0"}, //29
+        {"1"}, //30
+        {"2"}, //31
+        {"3"}, //32
+        {"4"}, //33
+        {"5"}, //34
+        {"6"}, //35
+        {"7"}, //36
+        {"8"}, //37
+        {"9"}, //38
+        {"p"}, //39
+        {"q"}, //40
+        {"r"}, //41
+        {"s"}, //42
+    }; 
+
 
     string input1 = inputs[2];
     cout <<input1 << endl;
