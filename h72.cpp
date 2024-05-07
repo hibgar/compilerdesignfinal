e5fd392ae7f25b3c6a355dfc4e77b6aa962165c4
Hiba Mughal <hibgar@gmail.com>
2024-05-06 19:32:44 -0700

    started logic

--- h72.cpp
+++ h72.cpp
@@ -8,19 +8,9 @@
 #include <unordered_map>
 using namespace std;
 
-unordered_map<char, int> indexMap = {
-    {'i', 0},
-    {'+', 1},
-    {'-', 2},
-    {'*', 3},
-    {'/', 4},
-    {'(', 5},
-    {')', 6},
-    {'$', 7}
-};
 
-void displayStack(stack<char> stack) {
-    vector<char> tempVector;
+void displayStack(stack<string> stack) {
+    vector<string> tempVector;
 
     // Copy elements from stack to tempVector
     while (!stack.empty()) {
@@ -29,9 +19,9 @@ void displayStack(stack<char> stack) {
     }
 
     // Display elements from tempVector
-    cout << endl;
+    std::cout << endl;
     for (auto it = tempVector.rbegin(); it != tempVector.rend(); ++it) {
-        cout << *it;
+        std::cout << *it;
     }
 
     // Restore elements to the original stack
@@ -53,24 +43,19 @@ std::vector<std::string> tokenize(const std::string& str) {
 
 int main() {
 
-    std::ifstream inputFile("finalv24.txt");
+    std::ifstream inputFile("final24.txt");
     if (!inputFile.is_open()) {
         std::cerr << "Error: Unable to open input file." << std::endl;
         return 1;
     }
 
-    std::string line;
-    while (std::getline(inputFile, line)) {
-        // Tokenize each line
-        std::vector<std::string> tokens = tokenize(line);
-        // Output each token
-        for (const auto& token : tokens) {
-            std::cout << token << std::endl;
-        }
+    std::vector<std::string> keys = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "p", "q", "r", "s", "+", "-", "*", "/", "=", "l", "program", "var", ";", ":", "(", ")", "$", "begin", "end", "integer", "write", "\"value=\""};
+    std::unordered_map<std::string, int> indexMap;
+
+    // Populate the map
+    for (int i = 0; i < keys.size(); ++i) {
+        indexMap[keys[i]] = i;
     }
-  
-    stack<char> compiler;
-    string inputs[3] = {"(i+i)*i$", "i*(i-i)$", "i(i+i)$"};
 
     int twodarray[23][33] = 
     {
@@ -99,7 +84,6 @@ int main() {
         {29,30,31,32,33,34,35,36,37,38,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}, //X
         {0,0,0,0,0,0,0,0,0,0,39,40,41,42,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0} //L
     };
- // 0: reject, 1: TQ, 2: +TQ, 3: -TQ, 4: lambda, 5: FR, 6: *FR, 7: /FR, 8: i, 9: (E)
 
 
     vector<vector<string>> key =
@@ -149,77 +133,75 @@ int main() {
         {"s"}, //42
     }; 
 
-
-    string input1 = inputs[2];
-    cout <<input1 << endl;
-    compiler.push('$');
-    compiler.push('E');
+    stack<string> compiler;
+    compiler.push("$");
+    compiler.push("E");
     
-    int inputNum = 0;
-    char currentRead;
+    
+    string currentRead;
     
 
     while (!compiler.empty()) {
         
         //pop
-        char val = compiler.top();
+        string val = compiler.top();
         compiler.pop();
 
-        //read string val
-        currentRead = input1[inputNum];
-        
-        //while no match
-        while (val != currentRead) {
-            // go to [_,_] -- determine array value based on val
-            
-            int arrayValue;
-            switch (val) {
-                case 'E':
-                    arrayValue = twodarray[0][indexMap[currentRead]];
-                    break;
-                case 'Q':
-                    arrayValue = twodarray[1][indexMap[currentRead]];
-                    break;
-                case 'T':
-                    arrayValue = twodarray[2][indexMap[currentRead]];
-                    break;
-                case 'R':
-                    arrayValue = twodarray[3][indexMap[currentRead]];
-                    break;
-                case 'F':
-                    arrayValue = twodarray[4][indexMap[currentRead]];
-                    break;
-            }
+        //read token
+        std::string line;
+        while (std::getline(inputFile, line)) {
+            // Tokenize each line
+            std::vector<std::string> tokens = tokenize(line);
+            // for each token
+            for (const auto& token : tokens) {
+                std::cout << token << std::endl;
+                currentRead = token;
 
-            if (arrayValue == 0) {
-                cout << endl <<"rejected" << endl;
-                return 0;
-            }
 
-            // push elements from key[arrayValue] into compiler
-            for (size_t i = 0; i < (sizeof(key[arrayValue-1]) / sizeof(key[arrayValue-1][0])); ++i) {
-                compiler.push(key[arrayValue-1][i]);
-            }
-            displayStack(compiler);
-            
-            
-            while ( compiler.top() == 0 or compiler.top() == 'l') {
-                compiler.pop();
-            }
-            val = compiler.top();
-            compiler.pop();
+                //while no match
+                while (val != currentRead) {
+                    
+                    // go to [_,_] -- determine array value based on val
+                    
+                    int arrayValue;
+                    if (val == "P") {
+                        arrayValue = twodarray[0][indexMap[currentRead]];
+                    } else if (val == "I") {
+                        arrayValue = twodarray[1][indexMap[currentRead]];
+                    } 
+                    
 
-            
+                    if (arrayValue == 0) {
+                        std::cout << endl << "rejected" << endl;
+                        return 0;
+                    }
+
+                    // push elements from key[arrayValue] into compiler
+                    for (size_t i = 0; i < (sizeof(key[arrayValue-1]) / sizeof(key[arrayValue-1][0])); ++i) {
+                        compiler.push(key[arrayValue-1][i]);
+                    }
+                    displayStack(compiler);
+                    
+                    
+                    while ( compiler.top() == "0" or compiler.top() == "l") {
+                        compiler.pop();
+                    }
+                    val = compiler.top();
+                    compiler.pop();
+
+                }
+
+                displayStack(compiler);
+
+                
+
+            }
         }
-
-        displayStack(compiler);
-
-        inputNum++;
-        currentRead = input1[inputNum];
+        
         
     }
 
-    cout << endl << "accepted"<< endl;
+    std::cout << endl << "accepted" << endl;
 
   return 0;
 }
